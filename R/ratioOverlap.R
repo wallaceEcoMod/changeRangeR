411bfb53cc1a6a03be3d273b88486253da77e4b7
Peter Galante <pgalante@amnh.org>
2020-05-14 16:45:23 -0400

    ratioOverlap addn

--- R/ratioOverlap.R
+++ R/ratioOverlap.R
@@ -2,10 +2,11 @@
 #' @description Calculate the proportion of the species' range (e.g., a thresholded SDM) that is contained by landcover categories
 #' taken from a shapefile. Example shapefile categories include protected areas, threatened areas. ratioOverlap returns an s4 object
 #' containing the masked raster layer and the percent of the total range that lies within the shapefile polygons specified.
-#' @param r Binary Raster
-#' @param shp a shapefile of land cover features
-#' @param category a list of the names of shapefile features to compare.
-#' @param proj proj4string of crs of landcover layer
+#' @param r Binary Raster.
+#' @param shp a shapefile of land cover features.
+#' @param field The shapefile field attribute containing the features to compare (i.e., the column name).
+#' @param category a list of the names of shapefile features to compare. If all features are to be used, input "All".
+#' @param proj character string proj4string of crs of landcover layer.
 #' @export
 #'
 #'
@@ -16,23 +17,33 @@ ratioOverlap <- function(r, shp, field, category){
   require(raster)
   require(dplyr)
 
-  shp <- st_as_sf(shp)
-  # if (crs == NULL){
-  #   fc <- filter(shp, grepl(category, field))
-  #   out<- mask(r, fc)
-  # } else {
-  r <- projectRaster(r, crs = crs(shp))
-  fc <- lapply(category, function(x) filter(shp, shp[[field]]==x))
-  fc <- do.call("rbind", fc)
-  #fc <- filter(shp, shp[[field]]==category)
-  maskedRange <- mask(r, fc)
-  #  }
-  #  return(out)
+  if (category == "All"){
+    shp <- st_as_sf(shp)
+    r <- projectRaster(r, crs = crs(shp))
+    maskedRange <- mask(r, shp)
+  }
+  else {
+    shp <- st_as_sf(shp)
+    # if (crs == NULL){
+    #   fc <- filter(shp, grepl(category, field))
+    #   out<- mask(r, fc)
+    # } else {
+    r <- projectRaster(r, crs = crs(shp))
+    fc <- lapply(category, function(x) filter(shp, shp[[field]]==x))
+    fc <- do.call("rbind", fc)
+    #fc <- filter(shp, shp[[field]]==category)
+    maskedRange <- mask(r, fc)
+    #  }
+    #  return(out)
+  }
   ratio <- ncell(maskedRange[!is.na(maskedRange)]) / ncell(r[!is.na(r)]) * 100
   ratio <- paste0("Percentage of range within shape is ", ratio, "%")
 
   out <- new("ratioOverlap", maskedRange = maskedRange, ratio = ratio)
 }
 
-
-
+# r <- raster('C:/Users/pgalante/Projects/NASA/maskRangeR/dataDriven/olinguito/olinguito_sdm.tif')
+# shp <- readOGR("C:/Users/pgalante/OneDrive - AMNH/NASA-Wallace/changeRangeR/singleSpecies/Olinguito_data/Olinguito_RS_data/WDPA_protected_areas/WDPA_May2020_COL-shapefile", "WDPA_May2020_COL-shapefile-polygons")
+# field <- "DESIG_ENG"
+# category <- "All"
+# test <- ratioOverlap(r = r, shp = shp, field = field, category = category)
