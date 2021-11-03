8080af8c385467b7e12009de66b31b673cabe9f0
Pgalante <pgalante@amnh.org>
2021-11-03 16:32:26 -0400

    rmm and documentation

--- R/cRRrmm.R
+++ R/cRRrmm.R
@@ -11,10 +11,54 @@
 #'
 
 
-buildRMM <- function(rmm=NULL){
+buildRMM <- function(binaryRange = NULL, rmm=NULL, locs = NULL, AOOarea=NULL, PE = NULL, PhyloTree = NULL,
+                     complementarity = NULL, complementarity.of = NULL, complementarity.mask = NULL,
+                     enChange = NULL, envChange.rStack = NULL, envChange.binaryRange = NULL, envChange.threshold=NULL,
+                     envChange.bound = NULL, envChange.correlation = NULL, futureOverlap.binRasters = NULL,
+                     futureOverlap.futures = NULL, mcp = NULL, mcpSDM = NULL, ratioOverlap = NULL, ratioOverlap.shape = NULL,
+                     ratioOverlap.field = NULL, SE = NULL, SE.ranges = NULL){
   if(is.null(rmm)) {
     rmm <- rangeModelMetadata::rmmTemplate()
   }
 
+  # AOOarea
+  rmm$postprocess$inputs$binarySDM <- r
+  rmm$postprocess$inputs$occurrences <- locs
+  rmm$postprocess$AOO$Area <- AOOarea$area
+  rmm$postprocess$AOO$raster <- AOOarea$aooRaster
+  rmm$postprocess$AOO$withOccs <- AOOarea$aooPixels
+  # phylogenetic endemism
+  rmm$multispecies$PE <- PE
+  rmm$multispecies$inputs$Phylogenetic_tree <- PhyloTree
+  # complementarity
+  rmm$postprocess$inputs$complementarity$type <- complementarity.of
+  rmm$postprocess$inputs$complementarity$mask <- complementarity.mask
+  rmm$postprocess$complementarity$withinMask <- complementarity$Percent_of_Total
+  rmm$postprocess$complementarity$outsideMask <- complementarity$Percent_unique_values
+  # envChange
+  rmm$postprocess$inputs$TemporalSDM$rStack <- envChange.rStack
+  rmm$postprocess$inputs$TemporalSDM$binaryRange <- envChange.binaryRange
+  rmm$postprocess$inputs$TemporalSDM$threshold <- envChange.threshold
+  rmm$postprocess$inputs$TemporalSDM$bound <- envChange.bound
+  rmm$postprocess$inputs$TemporalSDM$correlation <- envChange.correlation
+  rmm$postprocess$TemporalSDM$Area <- envChange$allAreas
+  rmm$postprocess$TemporalSDM$masks <- envChange$masks
+  # futureOverlap
+  rmm$postprocess$inputs$TemporalOverlap$binaryRasters <- futureOverlap.binRasters
+  rmm$postprocess$inputs$TemporalOverlap$futures <- futureOverlap.futures
+  rmm$postprocess$TemporalOverlap$areaOverTime
+  # mcp
+  rmm$postprocess$mcp$mcpLocs <- mcp
+  rmm$postprocess$mcp$mcpSDM <- mcpSDM
+  # ratioOVerlap
+  rmm$postprocess$inputs$binaryRange <- binaryRange
+  rmm$postprocess$inputs$overlap.shape <- ratioOverlap.shape
+  rmm$postprocess$inputs$overlap.field <- ratioOverlap.field
+  rmm$postprocess$overlap$maskedRange <- ratioOverlap$maskedRange
+  rmm$postprocess$overlap$rangeInCategory <- ratioOverlap$ratio
+  rmm$postprocess$overlap$correlation <- ratioOverlap$correlation
+  #Species endemism
+  rmm$multispecies$inputs$ranges <- SE.ranges
+  rmm$multispecies$SE <- SE
 }
 
